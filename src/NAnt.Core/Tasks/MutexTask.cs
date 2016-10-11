28857b1d00f3beea154be7c73fdba77a16c4954a
nathanscottdaniels <nathan@nathandaniels.net>
2016-10-11 00:08:09 -0400

    A little cleanup and error handling and making parallel run in sequence if needed

--- src/NAnt.Core/Tasks/MutexTask.cs
+++ src/NAnt.Core/Tasks/MutexTask.cs
@@ -4,6 +4,11 @@ using System.Threading;
 
 namespace NAnt.Core.Tasks
 {
+    /// <summary>
+    /// The mutex task will execute its contents once it obtains a handle on the system mutex specified
+    /// by <see cref="Name">name</see>.  In order to prevent endless builds, the build will fail if the
+    /// mutex is not obtained within <see cref="Timeout"/> seconds.
+    /// </summary>
     [TaskName("mutex")]
     public class MutexTask : TaskContainer
     {
@@ -12,12 +17,12 @@ namespace NAnt.Core.Tasks
         /// </summary>
         [TaskAttribute("name", ExpandProperties = true, Required = true)]
         [StringValidator(AllowEmpty = false)]
-        public String Name { get; set; }
+        public new String Name { get; set; }
 
         /// <summary>
         /// The amount of time to wait for the mutext to become available before failing the build
         /// </summary>
-        [TaskAttribute("timeout")]
+        [TaskAttribute("timeout", Required = true)]
         [Int32Validator(MinValue = 1)]
         public Int32 Timeout { get; set; } = -1;
 
@@ -31,6 +36,39 @@ namespace NAnt.Core.Tasks
         /// </summary>
         protected override void ExecuteTask()
         {
+            this.CheckForDeadlocks();
+
+            if (!this.chosenMutex.WaitOne(new TimeSpan(0, 0, this.Timeout)))
+            {
+                throw new BuildException($"Timeout expired while waiting for mutext \"{this.Name}\".  The timeout was {this.Timeout} seconds.");
+            }
+
+            try
+            {
+                base.ExecuteTask();
+            }
+            finally
+            {
+                this.chosenMutex.ReleaseMutex();
+            }
+        }
+
+        /// <summary>
+        /// Initializes this task
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            this.chosenMutex = new Mutex(false, this.Name);
+        }
+
+        /// <summary>
+        /// Attempts to check for possible deadlocks by looking up the call chain to see if a parent is holding this mutex
+        /// </summary>
+        /// <exception cref="BuildException">If a deadlock is detected</exception>
+        private void CheckForDeadlocks()
+        {
             for (var ancestor = this.Caller; ancestor != null; ancestor = ancestor.Caller)
             {
                 var task = ancestor as MutexTask;
@@ -42,28 +80,6 @@ namespace NAnt.Core.Tasks
                     }
                 }
             }
-
-            if (this.Timeout != -1)
-            {
-                this.chosenMutex.WaitOne(new TimeSpan(0, 0, this.Timeout));
-            }
-            else
-            {
-                this.chosenMutex.WaitOne();
-            }
-
-            base.ExecuteTask();
-            this.chosenMutex.ReleaseMutex();
-        }
-
-        /// <summary>
-        /// Initializes this task
-        /// </summary>
-        protected override void Initialize()
-        {
-            base.Initialize();
-
-            this.chosenMutex = new Mutex(false, this.Name);
         }
     }
 }
--- src/NAnt.Core/Tasks/ParallelTask.cs
+++ src/NAnt.Core/Tasks/ParallelTask.cs
@@ -2,9 +2,7 @@
 using NAnt.Core.Types;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NAnt.Core.Tasks
@@ -18,6 +16,13 @@ namespace NAnt.Core.Tasks
         internal List<ParallelTarget> Targets { get; set; } = new List<ParallelTarget>();
 
         /// <summary>
+        /// If true, the targets within this parallel will not run in parallel but instead in order.
+        /// </summary>
+        [TaskAttribute("serial", Required = false)]
+        [BooleanValidator]
+        public Boolean RunInSerial { get; set; }
+
+        /// <summary>
         /// Defines a set of path elements to add to the current path.
         /// </summary>
         /// <param name="path">The <see cref="PathSet" /> to add.</param>
@@ -27,58 +32,73 @@ namespace NAnt.Core.Tasks
             this.Targets.Add(path);
         }
 
+        /// <summary>
+        /// Executes this task
+        /// </summary>
         protected override void ExecuteTask()
         {
             this.Log(Level.Info, "Begining parallel execution of targets...");
             this.Project.Indent();
 
-            try
+            var targets = this.Targets.Where(t => t.IfDefined && !t.UnlessDefined);
+
+            if (this.RunInSerial)
             {
-                Parallel.ForEach(this.Targets.Where(t => t.IfDefined && !t.UnlessDefined), (targetElement, state) =>
+                foreach (var targetElement in targets)
                 {
-                    try
-                    {
-                        this.Log(Level.Info, $"Executing \"{ targetElement.TargetName}\" in parallel.");
-
-                        if (!state.IsExceptional && !state.IsStopped)
-                        {
-                            this.Project.Execute(targetElement.TargetName, this);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        state.Stop();
-
-                        lock (this)
-                        {
-                            var message = $"ERROR: An exception has been thrown by the \"{targetElement.TargetName}\" target of the parallel.  Any currently executing targets will run to completion but the build will fail.";
-
-                            this.Log(Level.Error, new string('=', message.Length));
-                            this.Log(Level.Error, message);
-                            this.Log(Level.Error, e.Message);
-                            this.Log(Level.Error, new string('=', message.Length) + "\r\n");
-                            throw;
-                        }
-                    }
-                });
+                    this.Log(Level.Info, $"Executing \"{ targetElement.TargetName}\" in sequence.");
+                    this.Project.Execute(targetElement.TargetName, this);
+                }
             }
-            catch (AggregateException agge)
+            else
             {
-                foreach (var inner in agge.InnerExceptions)
+                try
                 {
-                    if (inner is BuildException)
+                    Parallel.ForEach(targets, (targetElement, state) =>
                     {
-                        throw inner;
+                        try
+                        {
+                            this.Log(Level.Info, $"Executing \"{ targetElement.TargetName}\" in parallel.");
+
+                            if (!state.IsExceptional && !state.IsStopped)
+                            {
+                                this.Project.Execute(targetElement.TargetName, this);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            state.Stop();
+
+                            lock (this) // Prevent ourself from jumbling our loging, at least
+                            {
+                                var message = $"ERROR: An exception has been thrown by the \"{targetElement.TargetName}\" target of the parallel.  Any currently executing targets will run to completion but the build will fail.";
+
+                                this.Log(Level.Error, new string('=', message.Length));
+                                this.Log(Level.Error, message);
+                                this.Log(Level.Error, e.Message);
+                                this.Log(Level.Error, new string('=', message.Length) + "\r\n");
+                                throw;
+                            }
+                        }
+                    });
+                }
+                catch (AggregateException agge)
+                {
+                    foreach (var inner in agge.InnerExceptions)
+                    {
+                        if (inner is BuildException)
+                        {
+                            throw inner;
+                        }
+                        else
+                        {
+                            throw new BuildException(inner.Message, inner);
+                        }
                     }
                 }
             }
 
             this.Project.Unindent();
-        }
-
-        protected override void Initialize()
-        {
-            base.Initialize();
         }
     }
 }
